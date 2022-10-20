9ab5443ee7183b7f843737464c51640102351458
BukharinovMaksym <mbukh2005@gmail.com>
2022-10-20 21:36:02 +0300

    first commit

---
+++ lab04_1/lab04_1.cpp
@@ -0,0 +1,45 @@
+﻿#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int k, N;
+	double S;
+
+	cout << "k = "; cin >> k;
+	
+	S = 0;
+	N = k;
+	while (N <= 19)
+	{
+		S += sqrt(sin(k) * sin(k) + cos(N / k) * cos(N / k));
+		N++;
+	}
+	cout << S << endl;
+
+	S = 0;
+	N = k;
+	do {
+		S += sqrt(sin(k) * sin(k) + cos(N / k) * cos(N / k));
+		N++;
+	} while (N <= 19);
+	cout << S << endl;
+
+	S = 0;
+	for (N = k; N <= 19; N++)
+	{
+		S += sqrt(sin(k) * sin(k) + cos(N / k) * cos(N / k));
+	}
+	cout << S << endl;
+
+	S = 0;
+	for (N = 19; N >= k; N--)
+	{
+		S += sqrt(sin(k) * sin(k) + cos(N / k) * cos(N / k));
+	}
+	cout << S << endl;
+
+	return 0;
+}
